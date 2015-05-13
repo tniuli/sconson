9d6734fd51ffbc89fb9005d746499fe1d611cf16
tniuli <tniuli@163.com>
2015-05-13 18:22:02 +0800

    add gtest

--- sconson/install.sh
+++ sconson/install.sh
@@ -5,6 +5,11 @@ cd deps/scons-2.3.4/
 python setup.py install
 cd -
 
+cd deps/
+rpm -Uvh gtest-1.0.0-1.x86_64.rpm
+cd -
+
+
 #install sconson
 mkdir /usr/local/sconson/ -p
 rm /usr/local/sconson/* -rf
